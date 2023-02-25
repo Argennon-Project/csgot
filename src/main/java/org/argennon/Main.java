d40435e3da2e420cedd85c896f02b7ac2fa1e445
aybehrouz <behrouz_ayati@yahoo.com>
2023-02-25 18:07:19 +0330

    Add `README.md`

--- src/main/java/org/argennon/Main.java
+++ src/main/java/org/argennon/Main.java
@@ -1,4 +1,6 @@
-package org.argennon;public class Main {
+package org.argennon;
+
+public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
